e638368312790f8e1e94ebd2ea8abf03eff305ad
altoidnerd <allenmajs1@gmail.com>
2015-09-11 08:07:52 -0400

    added v22

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 # bitcoin-data
-get bitcoin data
+
+Get bitcoin price data from the coinbase API and parse it into an easily manipulated form.
 
     >>> from fast_dump_v21 import *
-    >>> parse_data(get_page(1))[0]
-
-returns prices for page 1.
-
-    >>> parse_data(get_page(1))[1]
-
-returns timestamps associated with the previous output
-
-    >>> get_page(1)
 
 returns all the data from page 1.
 
@@ -18,4 +10,45 @@ You can almost always turn on optional debug statements
 
     >>> get_first_N(3, show=True)
     ... fetching first 3
+
+You can get all the price data that coinbase has
+
+    >>> get_all(show=True)
+    ... getting page 1
+    ... getting page 2
+    ... getting page 3
+    ... getting page 4
+    ... getting page 5
+    ... getting page 6
+    ... getting page 7
+    ... getting page 8
+    ... getting page 9
+    ... etc ...
+
+All the 'get\_\*' functions return a price_data string, which is interlaced timespams and prices littered with newlines and commas.  You can print them to see what is going on more clearly:
+
+    >>>> print(get_page(1))
+    2015-09-11T06:44:04-05:00,241.14
+    2015-09-11T06:34:04-05:00,240.8
+    2015-09-11T06:24:04-05:00,240.75
+    2015-09-11T06:14:05-05:00,240.68
+    2015-09-11T06:04:04-05:00,240.83
+    2015-09-11T05:54:05-05:00,240.92
+    2015-09-11T05:44:04-05:00,240.64
+    2015-09-11T04:34:04-05:00,241.27
+    2015-09-11T04:24:04-05:00,240.73
+
+The parse_data() function is there to separate these components. 
+
+    >>> parsed = parse_data(get_page(1))
+    >>> x[0][0]
+    '241.2'
+    >>> x[0][1]
+    '241.14'
+    >>> x[1][1]
+    '2015-09-11T04:34:04-07:00'
+
+As you can see, pase_data(price_data_[0][k] returns the kth price in the list.  Indices [1][k] return the kth timestamp.  
+
+
 
---
+++ fast_dump_v22.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import requests
+import sys
+
+def get_page(page_num):
+    price_data = ''
+    req = requests.get("http://coinbase.com/api/v1/prices/historical?page="+str(page_num))
+    if req.status_code == 200:
+        price_data += req.text
+    else:
+        price_data += "API error"
+    return price_data
+
+def get_first_N(first_N, show=False):
+    price_data = ''
+    k = 1 
+    if show: print(" ... fetching first "+str(first_N))
+    while k <= first_N:                                                                               
+        price_data +='\n' + get_page(k)
+        if show: print(" ... getting page " + str(k))
+        k+=1
+    return price_data
+
+# slightly better than get_first_N()?
+# right now page 149 happens to be the end
+def get_range(low_index=1, high_index=149, show=False):
+    price_data = ''
+    k = low_index
+    if show: print("... fetching pages "+str(low_index)+" through "+str(high_index+" ..."))
+    while k <= high_index:
+        price_data +='\n' + get_page(k)
+        if show == True: print("... getting page " + str(k))
+        k+=1
+    return price_data
+
+def get_all(show=False):
+    price_data = ''
+    page_num = 1
+    while True:
+        new_page = get_page(page_num)
+        price_data += '\n' + new_page
+        if show: print("... getting page " + str(page_num))
+        page_num += 1
+        if new_page == "":
+            break
+    return price_data
+
+def parse_data(price_data, show=False):
+    formatted = price_data.replace(',','\n').split('\n')
+    prices = formatted[::2]
+    timestamps = formatted[1::2]
+    vectors = [timestamps, prices]
+    if show:
+        print(vectors)
+    return vectors 
+
+if __name__ == '__main__':
+    sys.stdout.write(get_all(show=True))        
+  

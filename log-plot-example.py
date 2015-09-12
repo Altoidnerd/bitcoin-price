5fa1b038856224124ab8ac1ac8fabdf10dce5860
Altoidnerd <allenmajs1@gmail.com>
2015-09-12 00:21:12 -0400

    Create log-plot-example.py

---
+++ log-plot-example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3                                                                                                              
+ 
+from price_dump import *
+import matplotlib.pyplot as plt
+import math
+ 
+data  = get_first_N(147) 
+price = prices(data) 
+ 
+# prices(data) is a list of numeric strings like
+# ['241.11', '241.01', ...] so we need to convert the 
+# those list entries to floats in order to take the log
+ 
+float_price = map(float, price)
+logs        = list(map(math.log10(float_price)))
+bins        = range(0, len(price))
+ 
+plt.plot(bins, logs[::-1])
+plt.show()

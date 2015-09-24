4e1fc3f21a894c91fabcb56128105b8ad5702670
altoidnerd <allenmajs1@gmail.com>
2015-09-24 14:26:58 -0400

    set N=15 for examples

--- plot-example.py
+++ plot-example.py
@@ -3,7 +3,7 @@
 from price_dump import *
 import matplotlib.pyplot as plt
 
-data = get_first_N(147, show=True) # keep this number small to start.  Currently there are about 148 pages available.
+data = get_first_N(15, show=True) # keep this number small to start.  Currently there are about 148 pages available.
 
 price = prices(data)
 bins = range(0, len(price))
